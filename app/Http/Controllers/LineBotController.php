a9d600198aaa54cc9d983a993376700c51cf14a0
Paul Ho <hmj1026@GMAIL.COM>
2018-05-07 18:21:31 +0800

    Added

---
+++ app/Http/Controllers/LineBotController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Services\LineBotService;
+
+class LineBotController extends Controller
+{
+    private $linebotservice;
+
+    public function __construct(LineBotService $linebotservice)
+    {
+    	$this->linebotservice = $linebotservice;
+    }
+
+    public function handle(Request $request)
+    {
+    	
+    	$this->linebotservice->pushMessage('Test from laravel.');
+    } 
+}
