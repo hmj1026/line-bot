cd55662f1b454b08fce911146e1c4059f282ab71
jajack36 <jajack36@gmail.com>
2018-05-09 11:03:51 +0800

    test reply

--- app/Http/Controllers/LineBotController.php
+++ app/Http/Controllers/LineBotController.php
@@ -20,9 +20,8 @@ class LineBotController extends Controller
 
     public function handle(Request $request)
     {
-    	Log::channel('debug')->info($request->toArray());
 
-    	$this->linebotservice->replyMessage($request->message);
+    	$this->linebotservice->replyMessage($request);
     	// $this->linebotservice->pushMessage($request->message);
     }
 
--- app/Services/LineBotService.php
+++ app/Services/LineBotService.php
@@ -34,21 +34,21 @@ class LineBotService
         return $this->lineBot->pushMessage($this->lineUserID, $content);
     }
 
-    public function replyMessage()
+    public function replyMessage($request)
     {
-        // Log::channel('debug')->info();
+        foreach ($request->events as $event) {
+            $replyToken = $event->replyToken;
+            $content = $event->message->text;
+        }
 
         if(count($content) > 5){
         	$content = new TextMessageBuilder($content);
         }else{
         	$content = new TextMessageBuilder('Yeeeeeeeeeeee');
         }
-        
-        $event = new BaseEvent($content);
 
-        $replyToken = $event->getReplyToken();
 
-        // Log::channel('debug')->info($replyToken);
+        Log::channel('debug')->info($replyToken);
 
 
         return $this->lineBot->replyMessage($replyToken, $content);
